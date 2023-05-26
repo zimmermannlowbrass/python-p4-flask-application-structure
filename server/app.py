d7046c10091930cc9b051e3a5da5e6c59fdf46da
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-26 15:42:01 -0500

    completed labwork

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+    
